6ac44fa921af2e2c3859815b9fc3b960cdd8d18b
turtle-brothers <an35642@yahoo.co.jp>
2023-05-01 12:23:47 +0900

    password_maneger

---
+++ week_3-4/QUEST/README.md
@@ -0,0 +1,48 @@
+# パスワードマネージャー
+シェルスクリプトを使用して個人学習用に作成したパスワードを管理するパスワードマネージャーです。
+
+
+## インストール法、始め方
+ターミナル上で次のコマンドを実行して下さい。
+
+git clone https://github.com/masa-kogi/password_manager.git
+cd password_manager
+./password_manager.sh
+
+## 使い方
+シェルスクリプトを実行すると、以下のメニューが表示されます。
+
+パスワードマネージャーへようこそ！
+次の選択肢から入力してください(Add Password/Get Password/Exit)：
+Add Password が入力されると、サービス名、ユーザー名、パスワードの入力が求められ、入力された情報をファイルに保存して暗号化します。 Get Password が入力されると、サービス名の入力が求められ、入力されたサービスのサービス名、ユーザー名、パスワードが表示されます。 Exit が入力されると、プログラムが終了します。 Exit が入力されるまではプログラムは終了せず、「次の選択肢から入力してください(Add Password/Get Password/Exit)：」が繰り返されます。
+
+▼アウトプット
+
+パスワードマネージャーへようこそ！
+次の選択肢から入力してください(Add Password/Get Password/Exit)：
+
+# Add Password が入力された場合
+サービス名を入力してください：
+ユーザー名を入力してください：
+パスワードを入力してください：
+
+パスワードの追加に成功しました。
+次の選択肢から入力してください(Add Password/Get Password/Exit)：
+
+# Get Password が入力された場合
+サービス名を入力してください：
+## サービス名が保存されていなかった場合
+そのサービスは登録されていません。
+## サービス名が保存されていた場合
+サービス名：hoge
+ユーザー名：fuga
+パスワード：piyo
+
+次の選択肢から入力してください(Add Password/Get Password/Exit)：
+
+# Exit が入力された場合
+Thank you!
+## プログラムが終了
+
+# Add Password/Get Password/Exit 以外が入力された場合
+入力が間違っています。Add Password/Get Password/Exit から入力してください
---
+++ week_3-4/password_manager.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+echo 'パスワードマネージャーへようこそ！'
+echo '次の選択肢から入力してください(Add Password/Get Password/Exit): '
+
+PASSWORD_FILE="password.txt"
+extension=".gpg"
+ENCRYPTED_PASSWORD_FILE+="${PASSWORD_FILE}${extension}"
+gpg_pass=$(cat gpg_password)
+
+while :
+do
+  read selected_option
+  case "$selected_option" in
+    "Add Password")
+      read -p 'サービス名を入力してください： ' service_name
+      read -p 'ユーザー名を入力してください： ' user_name
+      read -p 'パスワードを入力してください： ' password
+      echo "$gpg_pass" | gpg --passphrase="$gpg_pass" --batch --yes --quiet -o "$PASSWORD_FILE" -d "$ENCRYPTED_PASSWORD_FILE"
+      echo $service_name:$user_name:$password >> "$PASSWORD_FILE"
+      echo 'パスワードの追加に成功しました。'
+      echo "$gpg_pass" | gpg --passphrase="$gpg_pass" --batch --yes -c "$PASSWORD_FILE"
+      rm "$PASSWORD_FILE"
+      echo '次の選択肢から入力してください(Add Password/Get Password/Exit): '
+      ;;
+    "Get Password")
+      read -p 'サービス名を入力してください： ' service_name
+      echo "$gpg_pass" | gpg --passphrase="$gpg_pass" --batch --yes --quiet -o "$PASSWORD_FILE" -d "$ENCRYPTED_PASSWORD_FILE"
+      if grep -q "^${service_name}" "$PASSWORD_FILE"; then
+          array=($(tac "$PASSWORD_FILE" | grep -m 1 "^${service_name}"))
+          array_split=(${ array//:/ })
+          echo "service_name:${array_split[0]}"
+          echo "user_name:${array_split[1]}"
+          echo "password:${array_split[2]}"
+      else
+          echo "そのサービスは登録されていません。"
+      fi
+      echo "$gpg_pass" | gpg --passphrase="$gpg_pass" --batch --yes -c "$PASSWORD_FILE"
+      rm "$PASSWORD_FILE"
+      echo '次の選択肢から入力してください(Add Password/Get Password/Exit): '
+      ;;
+    "Exit")
+      echo 'Thank you!'
+      break
+      ;;
+    *)
+      echo '入力が間違っています。Add Password/Get Password/Exit から入力してください。'
+  esac
+done
